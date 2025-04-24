cecef7cb012299cb783c8bfdd9b70eb28b8c0600
池上翔太 <>
2025-04-24 15:53:48 +0900

    プログラムページ作成

---
+++ page-program.php
@@ -0,0 +1,241 @@
+<?php get_header(); ?>
+
+<div class="pageService _transition">
+  <div class="mv _page pageService_mv pageServiceMv">
+    <picture class="mv_thumbnail pageServiceMv">
+      <source media="(max-width:768px)"
+        srcset="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/continuation/mv_img_sp.png">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/continuation/mv_img.png"
+        alt="ホワイトボードに文字を書いて就労支援について解説する女性">
+    </picture>
+
+    <div class="mv_content">
+      <h1 class="mv_heading" data-entitle="SERVICE">就労継続支援</h1>
+      <p class="mv_subcopy">特性に理解のある職場で働きたい方へ </p>
+      <p class="mv_text text">LUMO+が運営するお弁当屋さんで<br class="sp-db">働く機会を提供します。</p>
+    </div>
+    <div class="mv_point _point1 pointbar">
+      <div class="pointbar_bar _blue" data-bar1="64"></div>
+      <div class="pointbar_bar _green" data-bar2="41"></div>
+    </div>
+    <div class="mv_point _point2 pointbar">
+      <div class="pointbar_bar _green" data-bar4="122"></div>
+      <div class="pointbar_bar _blue" data-bar3="42"></div>
+    </div>
+  </div>
+  <!-- /.mv pageService_mv -->
+
+  <div class="pageService_content page_content">
+
+    <section class="pageService_lead pageServiceLead">
+      <div class="inner">
+        <div class="pageServiceLead_box">
+          <div class="pageServiceLead_inner">
+            <h2 class="pageServiceLead_heading heading _sub _dashed _center"><span class="heading_text"><span class="pc-dib">就労継続支援は</span>こんな方が利用しています</span></h2>
+            <p class="pageServiceLead_text">※18歳以上65歳未満の障がいを持っている方が対象となります。</p>
+            <ul class="pageServiceLead_list list _card _blue">
+              <li class="list_item">一般企業で働くことが難しい、<br>または不安がある</li>
+              <li class="list_item">発達障がいで対人関係が苦手な<br class="pc-db">ため、理解のある職場で働きたい</li>
+              <li class="list_item">精神疾患があり、体力面で<br>長時間働くことが難しい</li>
+              <li class="list_item">将来飲食店で働くことを目標に<br>職業訓練をしたい</li>
+            </ul>
+          </div>
+          <div class="pageServiceLead_point _point1 pointbar">
+            <div class="pointbar_bar _blue"></div>
+            <div class="pointbar_bar _green"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.pageService_lead -->
+
+    <section class="pageService_about pageServiceAbout">
+      <div class="inner">
+        <h2 class="pageServiceAbout_heading heading _bar _blue _m">LUMO+の<br class="pc-db">就労継続支援<span class="pc-dib">プログラム</span><span class="small pc-dib"> （A型）</span></h2>
+
+        <div class="pageServiceAbout_list">
+          <div class="pageServiceAbout_listItem">
+            <div class="pageServiceAbout_body">
+              <h3 class="pageServiceAbout_subheading heading _sub">障がいに理解のある職場で<br>職業訓練をすることができます</h3>
+              <p class="pageServiceAbout_text text">LUMO＋が運営するお弁当屋さんで、<span class="bold">対人関係や体力面などでのサポートを受けながら働くことができます。</span>（雇用契約・賃金のお支払いあり）<br>業務内容は、お弁当の仕込み、盛り付け、配達補助など様々で、<span class="bold">将来の就職に役立つ経験を積むことができます。</span>また、配達補助を通してお客さまから直接「ありがとう」をもらえる、やりがいのある仕事です。</p>
+            </div>
+            <div class="pageServiceAbout_img">
+              <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/transition/service_img01.jpg" alt="お弁当やさんが老人にお弁当を手渡ししている様子">
+            </div>
+          </div>
+        </div>
+
+        <div class="pageServiceAbout_flow">
+          <h3 class="pageServiceAbout_heading heading _m">1日の流れ</h3>
+          <div class="pageServiceAbout_box _flow">
+            <div class="pageServiceAbout_flowContent">
+              <div class="pageServiceAbout_flowImg">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/transition/flow_img.jpg" alt="お弁当の仕込み・盛り付け作業をしている様子">
+
+              </div>
+              <dl class="pageServiceAbout_timelist timeList">
+                <div class="timeList_item">
+                  <dt class="timeList_dt">8:00</dt>
+                  <dd class="timeList_dd">出社・朝礼</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">8:15</dt>
+                  <dd class="timeList_dd">お弁当の作成<span class="pc-dib">・</span><br class="sp-db">配達準備</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">12:00</dt>
+                  <dd class="timeList_dd">昼食休憩</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">12:30</dt>
+                  <dd class="timeList_dd">お弁当調理<span class="pc-dib">・</span><br class="sp-db">おかずのカップ盛り</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">15:00</dt>
+                  <dd class="timeList_dd">清掃</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">15:00</dt>
+                  <dd class="timeList_dd">終礼</dd>
+                </div>
+                <div class="timeList_item">
+                  <dt class="timeList_dt">15:30</dt>
+                  <dd class="timeList_dd">退社</dd>
+                </div>
+              </dl>
+            </div>
+            <div class="pageServiceAbout_point _point1 pointbar">
+              <div class="pointbar_bar _blue"></div>
+              <div class="pointbar_bar _green"></div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- /.pageService_feaure pageServiceAbout -->
+
+    <section class="pageServiceGuide" id="guide">
+      <div class="inner">
+        <h2 class="pageServiceFeature_heading heading _m _bar _center"><span></span>ご利用案内</h2>
+
+        <div class="guide pageServiceGuide_guide">
+          <p class="guide_heading">尼崎事業所</p>
+          <div class="guide_content ">
+            <div class="guide_body">
+              <dl class="guide_list">
+                <div class="guide_item">
+                  <dt class="guide_dt">アクセス</dt>
+                  <dd class="guide_dd">
+                    <p class="cta_text">兵庫県尼崎市大庄北４丁目２３−２７</p>
+                    <a href="https://goo.gl/maps/xroTeN6Pm43FEdJJ7" class="cta_link _map" target="_blank">Googleマップで見る</a>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">電話番号</dt>
+                  <dd class="guide_dd">
+                    <p>06-6439-6789</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">営業時間</dt>
+                  <dd class="guide_dd">
+                    <p>7:00-18:00</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">定休日</dt>
+                  <dd class="guide_dd">
+                    <p>土日祝日、年末年始</p>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+            <div class="guide_map map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204.93765607527186!2d135.3873091144358!3d34.73033143427753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000ee1a6b12c509%3A0x3feb61bd12e3f142!2z44CSNjYwLTAwNjMg5YW15bqr55yM5bC85bSO5biC5aSn5bqE5YyX77yU5LiB55uu77yS77yT4oiS77yS77yX!5e0!3m2!1sja!2sjp!4v1689070703929!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="guide pageServiceGuide_guide">
+          <p class="guide_heading">吹田事業所</p>
+          <div class="guide_content ">
+            <div class="guide_body">
+              <dl class="guide_list">
+                <div class="guide_item">
+                  <dt class="guide_dt">アクセス</dt>
+                  <dd class="guide_dd">
+                    <p class="cta_text">大阪府吹田市末広町26-4</p>
+                    <a href="https://x.gd/FYjCe" class="cta_link _map" target="_blank">Googleマップで見る</a>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">電話番号</dt>
+                  <dd class="guide_dd">
+                    <p>06-7164-2280</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">営業時間</dt>
+                  <dd class="guide_dd">
+                    <p>7:00-17:00</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">定休日</dt>
+                  <dd class="guide_dd">
+                    <p>土日祝日、年末年始</p>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+            <div class="guide_map map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.5842013525953!2d135.534145!3d34.7660656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e38357ea77e3%3A0x5cbbf9e80818e8f6!2z44CSNTY0LTAwMjIg5aSn6Ziq5bqc5ZC555Sw5biC5pyr5bqD55S677yS77yW4oiS77yUIOOCq-OCteODquODmeODvOODqQ!5e0!3m2!1sja!2sjp!4v1720664259132!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="guide pageServiceGuide_guide">
+          <p class="guide_heading">伊丹事業所（申請前）</p>
+          <div class="guide_content ">
+            <div class="guide_body">
+              <dl class="guide_list">
+                <div class="guide_item">
+                  <dt class="guide_dt">アクセス</dt>
+                  <dd class="guide_dd">
+                    <p class="cta_text">兵庫県伊丹市梅ノ木６−２−１５</p>
+                    <a href="https://x.gd/nqm0i" class="cta_link _map" target="_blank">Googleマップで見る</a>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">電話番号</dt>
+                  <dd class="guide_dd">
+                    <p>072-767-9826</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">営業時間</dt>
+                  <dd class="guide_dd">
+                    <p>ー</p>
+                  </dd>
+                </div>
+                <div class="guide_item">
+                  <dt class="guide_dt">定休日</dt>
+                  <dd class="guide_dd">
+                    <p>土日祝日、年末年始</p>
+                  </dd>
+                </div>
+              </dl>
+            </div>
+            <div class="guide_map map">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6554.459693677019!2d135.410934!3d34.7749886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000f1b295ef6203%3A0x216e4121f1c09a40!2z44CSNjY0LTA4NTYg5YW15bqr55yM5LyK5Li55biC5qKF44OO5pyo77yW5LiB55uu77yS4oiS77yR77yV!5e0!3m2!1sja!2sjp!4v1720664764055!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <!-- /.pageServiceGuide -->
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
