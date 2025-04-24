05dc4aa974ffb334d16d8ea9d14ed56377fa400c
池上翔太 <>
2025-04-24 15:37:07 +0900

    色々追加したアバウトページ。あと「まずはお気軽にご相談ください」のところの動く画像が横に長くなっていたからoverflow:hidden;した

--- page-about.php
+++ page-about.php
@@ -133,6 +133,264 @@
               class="origin">ご相談はこちら</span></a></p>
       </div>
     </section>
+    <section class="section__reason">
+      <h2 class="pageServiceAbout_heading heading _m _center _bar">LUMO＋が選ばれる理由</h2>
+      <p class="p section__reason__p">
+        LUMO+は、一人ひとりが特性を活かしながら安心して就労を目指せる環境が整った就労移行支援施設です。新しい一歩を踏み出そうとするそのチャレンジに、スキルと自信をもってもらえるように、安心して通所いただける環境を整えております。
+      </p>
+      <div class="section__reason__1 section__reason__width">
+        <div class="section__reason__title">
+          <div class="section__reason__title__sub">選ばれる理由01</div>
+          <h3 class="section__reason__title__h3">長く働き続けるためのプログラム</h3>
+        </div>
+        <div class="section__reason__img">
+          <img class="img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason1.jpg" alt="">
+        </div>
+        <div class="section__reason__contents bg__green">
+          <div class="section__reason__content">
+            <div class="section__reason__content__wrap">
+              <h4 class="section__reason__content__h4">基礎スキル</h4>
+              <p class="p">
+                一人ひとりのレベルに合わせて、カスタムメイドでカリキュラムを作成します。ビジネス基礎スキルを学べるプログラムは400種類を超え、資格の取得も可能です。「こんなに基礎から教えてくれるの？」と言っていただけるほど、就労支援ならではの手厚いサポートが特徴です。安心して学べる環境で、自信がつくまで一緒に伴走いたします。
+              </p>
+            </div>
+            <div class="section__reason__content__img">
+              <img class="img2" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason1-1.png" alt="">
+            </div>
+          </div>
+          <div class="section__reason__content">
+            <div class="section__reason__content__wrap">
+              <h4 class="section__reason__content__h4">専門カリキュラム</h4>
+              <p class="p">
+                pixiv社と共同開発したカリキュラムは、デザイナーやクリエイターのスキル習得を目指すコンテンツです。プロが実際に使用するツール「Abobe」のIllustratorやPhotoshopのインストール方法からスタートし、操作方法だけでなく、クリエイター技術の全体を動画解説付きで学ぶことができる教材でスキル習得をサポートします。
+              </p>
+            </div>
+            <div class="section__reason__content__img">
+              <img class="img2" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason1-2.png" alt="">
+            </div>
+          </div>
+          <div class="section__reason__content">
+            <div class="section__reason__content__wrap">
+              <h4 class="section__reason__content__h4">運動プログラム</h4>
+              <p class="p">
+                10年に及ぶフィットネスの現場と運動療育教室の運営で得た、運動と脳の関係性に対する知見をもとに、医師と共同で運動プログラムを開発。「人との感じ方の違い」「緊張や気分の起伏」「ストレス」に対して、自分でコントロールできるように、脳に起こっているトラブルの原因にアプローチします。
+              </p>
+            </div>
+            <div class="section__reason__content__img">
+              <img class="img2" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason1-3.png" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="cva">
+          <h3 class="cva__h3">あなたの一歩をお待ちしています。</h3>
+          <p class="cva__p cta_button button _primary _bar _l js-button"><a
+              href="https://lumo-plus.jp/contact/" class="button_link"><span
+                class="origin">ご相談はこちら</span></a></p>
+        </div>
+      </div>
+      <div class="section__reason__2 section__reason__width">
+        <div class="section__reason__title">
+          <div class="section__reason__title__sub">選ばれる理由02</div>
+          <h3 class="section__reason__title__h3">医師監修の運動アプローチ</h3>
+        </div>
+        <div class="section__reason__img">
+          <img class="img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason2.jpg" alt="">
+        </div>
+        <div class="section__reason__2__contents bg__green">
+
+          <div class="section__reason__2__content">
+            <div class="section__reason__number">
+              <div class="section__reason__step">STEP</div>
+              <div class="section__reason__no">01</div>
+            </div>
+            <div>
+              <h4 class="section__reason__2__h4">日常的に運動を取り入れる</h4>
+              <p class="p">
+                LUMO+では、医師監修の運動プログラムを午前と午後の2回に分けて実施します。運動を習慣化することで、生活リズムが整い、心身の健康維持につながります。<br>
+                また、カラダを動かすことにポジティブになることで就労後も健康的な生活を送りやすくなり、安定したライフスタイルの土台を作ることができます。</p>
+            </div>
+          </div>
+          <div class="section__reason__2__content">
+            <div class="section__reason__number">
+              <div class="section__reason__step">STEP</div>
+              <div class="section__reason__no">02</div>
+            </div>
+            <div>
+              <h4 class="section__reason__2__h4">コンディションを整える</h4>
+              <p class="p">
+                適度な運動で血流を改善することで、疲労の軽減や睡眠の質の向上など、体調管理に役立ちます。デスクワークが多い職種では、長時間の座り姿勢による身体の不調が発生しやすく、日々のコンディションがとても重要です。<br>
+                また、体調が安定することで仕事のパフォーマンスが向上し、集中力や作業効率がアップします。
+              </p>
+            </div>
+          </div>
+          <div class="section__reason__2__content">
+            <div class="section__reason__number">
+              <div class="section__reason__step">STEP</div>
+              <div class="section__reason__no">03</div>
+            </div>
+            <div>
+              <h4 class="section__reason__2__h4">働く体力をつける</h4>
+              <p class="p">
+                働くためには、長時間の作業に耐えられる体力が必要です。特に、初めての就職やブランクがある場合、仕事に慣れるまでに体力が追いつかないことがあります。そのため、基礎体力の向上させ、疲れにくいカラダを作ることが必要です。<br>
+                就労前に運動を習慣化することで、安定した勤務を継続しやすくなります。
+              </p>
+            </div>
+          </div>
+          <div class="section__reason__2__content">
+            <div class="section__reason__number">
+              <div class="section__reason__step">STEP</div>
+              <div class="section__reason__no">04</div>
+            </div>
+            <div>
+              <h4 class="section__reason__2__h4">メンタルコントロール</h4>
+              <p class="p">
+                仕事では緊張やプレッシャーなどを感じる場面が多いため、日々の運動を通じてココロの余裕を持つことが大切です。職場での対人関係や業務に対しても、前向きな姿勢やメンタルを維持しやすくなります。<br>
+                また、運動で睡眠の質を向上させることで、疲れを翌日に持ち越さないカラダ作りにもつながります。
+              </p>
+            </div>
+          </div>
+          <div class="section__reason__list">
+            <h4 class="section__reason__2__h4">効果が期待できるお悩みリスト（一例）</h4>
+            <div class="section__reason__list__wrap">
+              <div class="section__reason__list__inner">
+                <ul class="check__ul">
+                  <li class="check__li">新しい場所や初対面の人に会うことはとても疲れる</li>
+                  <li class="check__li">突然の物音や大きな音で驚きやすい</li>
+                  <li class="check__li">こだわりが強く感情的になりやすいと感じる</li>
+                  <li class="check__li">決められたことを守るのが好き、または得意</li>
+                  <li class="check__li">突然のスケジュール変更はパニックになる</li>
+                  <li class="check__li">洋服の繊維に敏感である</li>
+                  <li class="check__li">掃除機/洗濯機/電車や遮断機の音が苦手</li>
+                  <li class="check__li">失敗が怖いと感じることがある</li>
+                  <li class="check__li">失敗を長く引きずってしまうことがある</li>
+                  <li class="check__li">散髪が嫌い</li>
+                  <li class="check__li">感情の起伏が大きいと感じる</li>
+                  <li class="check__li">集中力が低い方だと感じる</li>
+                </ul>
+              </div>
+              <div class="section__reason__list__img">
+                  <img class="img2" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason2-1.png" alt="">
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="cva">
+          <h3 class="cva__h3">あなたの一歩をお待ちしています。</h3>
+          <p class="cva__p cta_button button _primary _bar _l js-button"><a
+              href="https://lumo-plus.jp/contact/" class="button_link"><span
+                class="origin">ご相談はこちら</span></a></p>
+        </div>
+      </div>
+      <div class="section__reason__3">
+        <div class="section__reason__width">
+          <div class="section__reason__title">
+            <div class="section__reason__title__sub">選ばれる理由03</div>
+            <h3 class="section__reason__title__h3">太陽光が入るフロアと親身に寄り添う支援員</h3>
+          </div>
+        </div>
+        <div class="section__reason__width section__reason__width__3">
+          <div class="section__reason__3__wrap">
+            <div class="section__reason__3__img">
+              <img class="img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason3-1.jpg" alt="">
+            </div>
+            <div class="section__reason__3__inner">
+              <p class="section__reason__3__p">フロアがあるのはビルの一階。<br>
+                採光が多く取られていて、<br>
+                晴れの日は気持ちが落ち着く、<br>
+                いっぱいの太陽光が差します。</p>
+            </div>
+          </div>
+          <div class="section__reason__3__wrap">
+            <div class="section__reason__3__img">
+              <img class="img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason3-2.JPG" alt="">
+            </div>
+            <div class="section__reason__3__inner">
+              <p class="section__reason__3__p">床はあたたかみのある木目調に。<br>
+                自然とリラックスできるような<br>
+                空間で気持ちよく運動できます。</p>
+            </div>
+          </div>
+          <div class="section__reason__3__wrap">
+            <div class="section__reason__3__img">
+              <img class="img" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/renovation/reason3-3.JPG" alt="">
+            </div>
+            <div class="section__reason__3__inner">
+              <p class="section__reason__3__p">壁面はレンガ調のデザインで<br>
+                落ち着いた雰囲気に。<br>
+                まるでおしゃれなカフェで<br>
+                働いているような気分になれます。</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="pageService__therapy pageService__info">
+                <div class="inner pageService__therapy__inner">
+                    <h2 class="pageServiceAbout_heading heading _m _center _bar">店舗情報</h2>
+                    <div class="pageService__info__wrap">
+                        <div class="pageService__info__wrapper">
+                            <table class="pageService__info__table">
+                                <tr class="pageService__info__tr">
+                                    <th class="pageService__info__th">アクセス</th>
+                                    <td class="pageService__info__td">大阪府大阪市阿倍野区天王寺町南2-1-6<br>ヴェルデ阿倍野1F</td>
+                                </tr>
+                                <tr class="pageService__info__tr">
+                                    <th class="pageService__info__th">電話番号</th>
+                                    <td class="pageService__info__td">060-6770-5283</td>
+                                </tr>
+                                <tr class="pageService__info__tr">
+                                    <th class="pageService__info__th">営業時間</th>
+                                    <td class="pageService__info__td">10:00~16:00</td>
+                                </tr>
+                                <tr class="pageService__info__tr">
+                                    <th class="pageService__info__th">定休日</th>
+                                    <td class="pageService__info__td">日・祝・年末年始</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="pageService__info__map">
+                            <iframe class="pageService__info__map__inner"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13129.684019672377!2d135.5224167!3d34.6440692!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dd9626c7bc1f%3A0xadd9ab415dbe331e!2z5bCx5Yq056e76KGM5pSv5o-05LqL5qWt5omAIExVTU_vvIsg6Zi_5YCN6YeO!5e0!3m2!1sja!2sjp!4v1724395045589!5m2!1sja!2sjp"
+                                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+    <section class="home_cta cta _sub">
+    <div class="inner">
+      <h2 class="heading cta_heading"><span class="cta_headingText">あなたに合う<span class="green _pc"><span class="green _sp">支援方法</span>をご提案</span>します</span><span class="pc-dib">まずは</span>お気軽にご相談ください</h2>
+      <p class="cta_button button _primary _bar _l js-button"><a href="<?php echo esc_url(home_url('/')); ?>contact/" class="button_link"><span class="origin">ご相談はこちら</span></a></p>
+
+
+    </div>
+    <div class="homeCta_point _point1 pointbar">
+      <div class="pointbar_bar _green"></div>
+      <div class="pointbar_bar _blue"></div>
+    </div>
+    <div class="homeCta_point _point2 pointbar">
+      <div class="pointbar_bar _blue"></div>
+      <div class="pointbar_bar _green"></div>
+    </div>
+  </section>
+  <!-- /.home_cta cta -->
+
+  <div class="homeLoop" style="overflow: hidden;">
+    <div class="loop-js sliderImage_loop">
+      <ul class="sliderImage_list">
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img01.jpg" alt="ペンとノートを手に勉強をしている様子"></li>
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img02.jpg" alt="LUMO+阿倍野事業所内の机"></li>
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img03.jpg" alt="LUMO+阿倍野事業所内の書籍など"></li>
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img04.jpg" alt="タブレットを見ながら仕事をしている様子"></li>
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img05.jpg" alt="LUMO+阿倍野事業所内のキッチンの様子"></li>
+        <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img06.jpg" alt="お弁当を手渡ししている様子"></li>
+      </ul>
+    </div>
+  </div>
+  <!-- /.homeLoop -->
 
     <section class="pageAboutTrouble">
       <div class="inner">
