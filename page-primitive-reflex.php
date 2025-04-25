3f928a3e8b55fbf3be2b96a7d2e7905f0dd7d826
池上翔太 <>
2025-04-25 10:04:06 +0900

    原始反射について着手

---
+++ page-primitive-reflex.php
@@ -0,0 +1,274 @@
+<?php get_header(); ?>
+
+<div class="pageAbout">
+  <div class="mv _page pageAbout_mv pageAboutMv">
+    <picture class="mv_thumbnail pageServiceMv">
+      <source media="(max-width:768px)"
+        srcset="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/continuation/mv_img_sp.png">
+      <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/continuation/mv_img.png"
+        alt="ホワイトボードに文字を書いて就労支援について解説する女性">
+    </picture>
+
+    <div class="mv_content">
+      <h1 class="mv_heading" data-entitle="PROGRAMS">原始反射について</h1>
+      <p class="mv_subcopy">あなたに合わせたカスタムメイドのご支援</p>
+    </div>
+    <div class="mv_point _point1 pointbar">
+      <div class="pointbar_bar _blue" data-bar1="64"></div>
+      <div class="pointbar_bar _green" data-bar2="41"></div>
+    </div>
+    <div class="mv_point _point2 pointbar">
+      <div class="pointbar_bar _green" data-bar4="122"></div>
+      <div class="pointbar_bar _blue" data-bar3="42"></div>
+    </div>
+  </div>
+  <!-- /.mv pageAbout_mv -->
+
+  <div>
+
+    <!-- /.pageAbout_lead -->
+
+
+
+    <section class="section__program">
+      <h2 class="pageServiceAbout_heading heading _m _center _bar">運動を取り入れた独自のスキル特化型プログラム</h2>
+      <p class="p section__program__p">
+        LUMO＋は一人ひとりの特性に合わせてプログラムをカスタムできるカリキュラムを提供しています。まずは生活や通所をスムーズにするための運動、特性を引き起こしている可能性のある原始反射に着目したエクササイズを行い、働くためのカラダ・ココロづくりをします。その後に基礎・応用的なビジネススキルを習得できるもののほかに、デザインやイラストなどの専門的なプログラムを受講しながら、最適な就労を目指して働く力を身につけていきます。
+      </p>
+      <ol class="section__program__ol">
+        <li class="section__program__li">
+          <div class="section__program__li__title">
+            <div class="section__program__no">
+              <div class="section__program__no__program">program</div>
+              <div class="section__program__no__number">01</div>
+            </div>
+            <h3 class="section__program__h3">ビジネス基礎・応用スキル</h3>
+          </div>
+          <div class="section__program__wrap">
+            <div class="section__program__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/program1.jpg" alt="">
+            </div>
+            <div class="section__program__contents">
+              <h4 class="section__program__h4"><span class="orange">基礎から</span>はじめる。<br>
+                <span class="orange">自信がつくまで</span>学ぶことが重要。
+              </h4>
+              <p class="p">一人ひとりのレベルに合わせてカリキュラムを作成します。プログラムは400種類を超え、資格の取得も可能です。<br>
+                「こんなに基礎から教えてくれるの？」と言っていただけるほど、就労支援ならではの手厚いサポートが特徴です。安心して学べる環境で、自信がつくまで一緒に伴走いたします。
+              </p>
+              <div class="section__program__skill">
+                <h5 class="section__program__h5">習得が可能なスキル</h5>
+                <p class="p">Excel 、Word、、PowerPoint、Access、Outlook、MOS取得、HTML、CSS、JavaScript</p>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="section__program__li">
+          <div class="section__program__li__title">
+            <div class="section__program__no">
+              <div class="section__program__no__program">program</div>
+              <div class="section__program__no__number">02</div>
+            </div>
+            <h3 class="section__program__h3">専門スキル</h3>
+          </div>
+          <div class="section__program__wrap">
+            <div class="section__program__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/program2.jpg" alt="">
+            </div>
+            <div class="section__program__contents">
+              <h4 class="section__program__h4"><span class="orange">pixiv社</span>と共同開発の<br>
+                プログラムで<br>
+                <span class="orange">クリエイタースキル</span>を。
+              </h4>
+              <p class="p">
+                pixiv社と共同開発したカリキュラムは、デザイナーやクリエイターのスキル習得を目指すコンテンツです。プロが実際に使用するツール「Abobe」のIllustratorやPhotoshopのインストール方法からスタートし、操作方法だけでなく、クリエイター技術の全体を動画解説付きで学ぶことができる教材でスキル習得をサポートします。
+              </p>
+              <!-- <p class="homeService_button _s button _green _bar js-button"><a href="" class="button_link"><span
+                                    class="origin" style="font-size: 14px;">開講記念対談記事へ</span></a></p> -->
+            </div>
+          </div>
+        </li>
+        <li class="section__program__li">
+          <div class="section__program__li__title">
+            <div class="section__program__no">
+              <div class="section__program__no__program">program</div>
+              <div class="section__program__no__number">03</div>
+            </div>
+            <h3 class="section__program__h3">運動プログラム</h3>
+          </div>
+          <div class="section__program__wrap">
+            <div class="section__program__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/program3.JPG" alt="">
+            </div>
+            <div class="section__program__contents">
+              <h4 class="section__program__h4"><span class="orange">脳を刺激して</span>発達特性に<br>
+                アプローチする<br>
+                <span class="orange">医師監修</span>の運動プログラム。
+              </h4>
+              <p class="p">
+                pixiv社と共同開発したカリキュラムは、デザイナーやクリエイターのスキル習得を目指すコンテンツです。プロが実際に使用するツール「Abobe」のIllustratorやPhotoshopのインストール方法からスタートし、操作方法だけでなく、クリエイター技術の全体を動画解説付きで学ぶことができる教材でスキル習得をサポートします。
+              </p>
+            </div>
+          </div>
+        </li>
+      </ol>
+    </section>
+    <!-- -------------------インタビュー------------------- -->
+    <section class="section__interview bg__green">
+      <h2 class="pageServiceAbout_heading heading _m _center _bar">ご利用者様へのインタビュー</h2>
+      <ul class="section__interview__ul">
+        <li class="section__interview__li">
+          <div class="section__interview__img">
+            <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/interview1.JPG" alt="">
+          </div>
+          <div class="section__interview__messege">回り道だったけど、ポジティブな自分へ変わることができた。</div>
+          <div class="section__interview__name p">利用歴１０ヶ月橋本 さん（仮名）</div>
+        </li>
+        <li class="section__interview__li">
+          <div class="section__interview__img">
+            <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/interview2.JPG" alt="">
+          </div>
+          <div class="section__interview__messege">踏み出した一歩が、自分の活かし方を見つけるきっかけに。</div>
+          <div class="section__interview__name p">利用歴１０ヶ月橋本 さん（仮名）</div>
+        </li>
+      </ul>
+      <p class="cta_button button _primary _bar _l js-button"><a href="https://lumo-plus.jp/contact/"
+          class="button_link"><span class="origin">ご相談はこちら</span></a></p>
+    </section>
+
+
+    <!-- ---------------契約までの流れ--------------- -->
+    <section class="section__contract">
+      <h2 class="pageServiceAbout_heading heading _m _center _bar">契約までの流れ</h2>
+      <ol class="section__contract__ol">
+        <li class="section__contract__li">
+          <div class="section__contract__icon">
+            <div class="section__contract__icon__number green fw500">STEP01</div>
+            <div class="section__contract__icon__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/contract1.png" alt="">
+            </div>
+          </div>
+          <div class="section__contract__content">
+            <div class="section__contract__content__h3 fw600 nowrap">お問い合わせ</div>
+            <div class="section__contract__content__p p">電話はLINEまたはメールで受け付けております。<br>
+              お悩みのご相談やご本人さま以外でもお気軽にお問いわせください。</div>
+          </div>
+        </li>
+        <li class="section__contract__li">
+          <div class="section__contract__icon">
+            <div class="section__contract__icon__number green fw500">STEP02</div>
+            <div class="section__contract__icon__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/contract2.png" alt="">
+
+            </div>
+          </div>
+          <div class="section__contract__content">
+            <div class="section__contract__content__h3 fw600 nowrap">施設見学</div>
+            <div class="section__contract__content__p p">施設を自由にご見学いただき、通っていただくイメージをつけます。<br>
+              プログラムのご紹介や利用プランについてもお伺いいたします。</div>
+          </div>
+        </li>
+        <li class="section__contract__li">
+          <div class="section__contract__icon">
+            <div class="section__contract__icon__number green fw500">STEP03</div>
+            <div class="section__contract__icon__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/contract3.png" alt="">
+            </div>
+          </div>
+          <div class="section__contract__content">
+            <div class="section__contract__content__h3 fw600 nowrap">3Days体験</div>
+            <div class="section__contract__content__p p">実際のプログラムを３日間程度の期間で体験していただきます。<br>
+              家から通えるか、施設の雰囲気が合っているかなどをご検討ください。</div>
+          </div>
+        </li>
+        <li class="section__contract__li">
+          <div class="section__contract__icon">
+            <div class="section__contract__icon__number green fw500">STEP04</div>
+            <div class="section__contract__icon__img">
+              <img class="img" src="<?php echo get_template_directory_uri(); ?>/assets/img/renovation/contract4.png" alt="">
+
+            </div>
+          </div>
+          <div class="section__contract__content">
+            <div class="section__contract__content__h3 fw600 nowrap">契約手続き</div>
+            <div class="section__contract__content__p p">通ってみたいと感じた上で、ご不明点やご不安な点が解消されたら支援の企画をご一緒に立てて、通所のための手続きを行います。
+            </div>
+          </div>
+        </li>
+      </ol>
+    </section>
+    <!-- ---------------契約までの流れend--------------- -->
+    <!-- 店舗ページ -->
+    <section class="pageService__therapy pageService__info">
+      <div class="inner pageService__therapy__inner">
+        <h2 class="pageServiceAbout_heading heading _m _center _bar">店舗情報</h2>
+        <div class="pageService__info__wrap">
+          <div class="pageService__info__wrapper">
+            <table class="pageService__info__table">
+              <tr class="pageService__info__tr">
+                <th class="pageService__info__th">アクセス</th>
+                <td class="pageService__info__td">大阪府大阪市阿倍野区天王寺町南2-1-6<br>ヴェルデ阿倍野1F</td>
+              </tr>
+              <tr class="pageService__info__tr">
+                <th class="pageService__info__th">電話番号</th>
+                <td class="pageService__info__td">060-6770-5283</td>
+              </tr>
+              <tr class="pageService__info__tr">
+                <th class="pageService__info__th">営業時間</th>
+                <td class="pageService__info__td">10:00~16:00</td>
+              </tr>
+              <tr class="pageService__info__tr">
+                <th class="pageService__info__th">定休日</th>
+                <td class="pageService__info__td">日・祝・年末年始</td>
+              </tr>
+            </table>
+          </div>
+          <div class="pageService__info__map">
+            <iframe class="pageService__info__map__inner"
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13129.684019672377!2d135.5224167!3d34.6440692!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000dd9626c7bc1f%3A0xadd9ab415dbe331e!2z5bCx5Yq056e76KGM5pSv5o-05LqL5qWt5omAIExVTU_vvIsg6Zi_5YCN6YeO!5e0!3m2!1sja!2sjp!4v1724395045589!5m2!1sja!2sjp"
+              width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="home_cta cta _sub">
+      <div class="inner">
+        <h2 class="heading cta_heading"><span class="cta_headingText">あなたに合う<span class="green _pc"><span class="green _sp">支援方法</span>をご提案</span>します</span><span class="pc-dib">まずは</span>お気軽にご相談ください</h2>
+        <p class="cta_button button _primary _bar _l js-button"><a href="<?php echo esc_url(home_url('/')); ?>contact/" class="button_link"><span class="origin">ご相談はこちら</span></a></p>
+
+
+      </div>
+      <div class="homeCta_point _point1 pointbar">
+        <div class="pointbar_bar _green"></div>
+        <div class="pointbar_bar _blue"></div>
+      </div>
+      <div class="homeCta_point _point2 pointbar">
+        <div class="pointbar_bar _blue"></div>
+        <div class="pointbar_bar _green"></div>
+      </div>
+    </section>
+    <!-- /.home_cta cta -->
+
+    <div class="homeLoop" style="overflow: hidden;">
+      <div class="loop-js sliderImage_loop">
+        <ul class="sliderImage_list">
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img01.jpg" alt="ペンとノートを手に勉強をしている様子"></li>
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img02.jpg" alt="LUMO+阿倍野事業所内の机"></li>
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img03.jpg" alt="LUMO+阿倍野事業所内の書籍など"></li>
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img04.jpg" alt="タブレットを見ながら仕事をしている様子"></li>
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img05.jpg" alt="LUMO+阿倍野事業所内のキッチンの様子"></li>
+          <li class="sliderImage_img"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/home/loop_img06.jpg" alt="お弁当を手渡ししている様子"></li>
+        </ul>
+      </div>
+    </div>
+    <!-- /.homeLoop -->
+
+
+
+
+
+
+  </div>
+</div>
+
+<?php get_footer(); ?>
